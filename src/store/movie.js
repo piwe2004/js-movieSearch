b2b289e7a3688e00525c3e20e814f311a8e778ff
origin <piwe2004@gmail.com>
2023-04-21 15:57:51 +0900

    예외처리 작업

--- src/store/movie.js
+++ src/store/movie.js
@@ -5,7 +5,8 @@ const store = new Store({
     page:1,
     pageMax:1,
     movies:[],
-    loading:false
+    loading:false,
+    message : 'Serach for the movie title'
 })
 
 export default store
@@ -15,13 +16,24 @@ export const searchMovies = async page => {
     if (page === 1){
         // 재검색시 리스트 초기화
         store.state.movies = []
+        store.state.message = ''
     }
-    const res = await fetch(`https://www.omdbapi.com/?apikey=397d836c&s=${store.state.searchText}&page=${store.state.page}`);
-    const { Search, totalResults } = await res.json();
-    store.state.movies = [
-        ...store.state.movies,
-        ...Search
-    ]
-    store.state.pageMax = Math.ceil(Number(totalResults) / 10)
-    store.state.loading = false
+    try {
+        const res = await fetch(`https://www.omdbapi.com/?apikey=397d836c&s=${store.state.searchText}&page=${store.state.page}`);
+        const { Search, totalResults, Response, Error } = await res.json();
+        if(Response === 'True'){
+            store.state.movies = [
+                ...store.state.movies,
+                ...Search
+            ]
+            store.state.pageMax = Math.ceil(Number(totalResults) / 10)
+        }else {
+            store.state.message = Error
+        }
+    } catch (error) {
+        console.log('searchMovies error : ', error)
+    }finally{
+        store.state.loading = false
+    }
+        
 }
