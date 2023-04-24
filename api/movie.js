1878170a13f917296a90e6ca5a31410be271afb0
origin <piwe2004@gmail.com>
2023-04-24 14:12:21 +0900

    dotenv 작성

--- api/movie.js
+++ api/movie.js
@@ -1,10 +1,12 @@
 import fetch from 'node-fetch'
 
+const {APIKEY} = process.env
+
 export default async function handler(request, response){
     const {title, page, id} = JSON.parse(request.body)
     const url = id 
-        ? `https://omdbapi.com/?apikey=397d836c&i=${id}&plot=full`
-        : `https://omdbapi.com/?apikey=397d836c&s=${title}&page=${page}`;
+        ? `https://omdbapi.com/?apikey=${APIKEY}&i=${id}&plot=full`
+        : `https://omdbapi.com/?apikey=${APIKEY}&s=${title}&page=${page}`;
     const res = await fetch(url)
     const json = await res.json()
     response.status(200).json(json)
